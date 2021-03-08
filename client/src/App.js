dcdddfa3d27a86df0fdd4510f96f3d325f854257
Austen Sulikowski <Austenski@gmail.com>
2021-03-08 08:53:58 -0500

    Yelpington

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Restaurants from "./components/Restaurants";
 function App() {
   return (
-    <h1>Yelpington</h1>
+    <div id="Full">
+      <header id="main-title">Yelpington</header>
+      <Switch>
+        <Route exact path = "/" component={Home}/>
+        <Route path = "/Restaurants/:Id" component={Restaurants}/>
+      </Switch>
+    
+    </div>
   );
 }
 
--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 const public = path.resolve('./public')
 
-app.use(express.static('./public'));
+app.use(express.static('./client/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(public + '/index.html')
 })
 
+app.get('/API/Restaurants', (req, res) => {
+    res.sendFile(path.resolve('./API/Restaurants.json'))
+})
+
+app.get('/Location/:name', (req, res) => {
+    res.sendFile(path.resolve(`./API/${req.params.name}.json`))
+})
+
 app.get("/Restaurants/:id", (req,res)=>{
-    res.sendFile(public + "/restaurants.html")
+    res.sendFile(public + "/Restaurants.html")
 })
 
 
